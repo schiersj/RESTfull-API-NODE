290c630c7574c3d2a08074d922100cc13201affe
Jeff Schiers <schiersj@gmail.com>
2018-02-28 15:01:46 -0600

    extracted validation to a middleware function

--- routes/returns.js
+++ routes/returns.js
@@ -1,14 +1,13 @@
+const Joi = require('joi');
 const moment = require('moment');
 const {Rental} = require('../models/rental');
 const {Movie} = require('../models/movie');
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
+const validate = require('../middleware/validate');
 
-router.post('/', auth, async (req, res) => {
-  if (!req.body.customerId) return res.status(400).send('customerId not provided');
-  if (!req.body.movieId) return res.status(400).send('movieId not provided');
-
+router.post('/', [auth, validate(validateReturn)], async (req, res) => {
   const rental = await Rental.findOne({
     'customer._id': req.body.customerId,
     'movie._id': req.body.movieId
@@ -28,5 +27,14 @@ router.post('/', auth, async (req, res) => {
 
   res.status(200).send(rental);
 });
+
+function validateReturn(req) {
+  const schema = {
+    customerId: Joi.objectId().required(),
+    movieId: Joi.objectId().required()
+  };
+
+  return Joi.validate(req, schema);
+}
 
 module.exports = router;
