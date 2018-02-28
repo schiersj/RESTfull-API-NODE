0bd6b3209e20f342aab399e444e490e7408d6299
Jeff Schiers <schiersj@gmail.com>
2018-02-28 15:13:43 -0600

    created instance method return in the rental module

--- routes/returns.js
+++ routes/returns.js
@@ -1,5 +1,4 @@
 const Joi = require('joi');
-const moment = require('moment');
 const {Rental} = require('../models/rental');
 const {Movie} = require('../models/movie');
 const express = require('express');
@@ -13,16 +12,14 @@ router.post('/', [auth, validate(validateReturn)], async (req, res) => {
 
   if (rental.dateReturned) return res.status(400).send('return already processed');
 
-  rental.dateReturned = new Date();
-  const rentalDays = moment().diff(rental.dateOut, 'days');
-  rental.rentalFee = rentalDays * rental.movie.dailyRentalRate;
+  rental.return();
   await rental.save();
 
   await Movie.update({ _id: rental.movie._id }, {
     $inc: { numberInStock: 1 }
   });
 
-  res.status(200).send(rental);
+  res.send(rental);
 });
 
 function validateReturn(req) {
