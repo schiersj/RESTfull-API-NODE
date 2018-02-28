cbde995534de1fa515a72474697d77fce4c1d785
Jeff Schiers <schiersj@gmail.com>
2018-02-28 10:31:24 -0600

    removed auth requirement on rentals put endpoint

--- routes/rentals.js
+++ routes/rentals.js
@@ -23,7 +23,7 @@ router.get('/:id', validateObjectId, async (req, res) => {
   res.send(rental);
 });
 
-router.post('/', auth, async (req, res) => {
+router.post('/', async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
