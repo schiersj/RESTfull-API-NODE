3e13378c10c31f549bcef68285826a5367210e04
Jeff Schiers <schiersj@gmail.com>
2018-03-01 10:15:59 -0600

    added unit test for the user.create method

--- tests/unit/models/user.test.js
+++ tests/unit/models/user.test.js
@@ -2,6 +2,7 @@ const { User } = require('../../../models/user');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 describe('user.generateAuthToken', () => {
   it('should return a valid JWT', () => {
@@ -13,5 +14,18 @@ describe('user.generateAuthToken', () => {
     const token = user.generateAuthToken();
     const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
     expect(decoded).toMatchObject(payload);
+  });
+});
+
+describe('user.create', () => {
+  it('should return a user with hashed password', async () => {
+    const payload = {
+      name: '12345',
+      email: 'me@email.com',
+      password: '12345'
+    };
+    const user = await User.create(payload);
+    const validPassord = await bcrypt.compare(payload.password, user.password);
+    expect(validPassord).toBe(true);
   });
 });
